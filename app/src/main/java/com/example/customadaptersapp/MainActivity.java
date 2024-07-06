9823af8f8c190f8fa4b84eeab105ac5a29697780
Avi-Afi <avishek.s@finalatix.com>
2024-07-06 18:51:17 +0530

    Adapter App

---
+++ app/src/main/java/com/example/customadaptersapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.customadaptersapp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 1 - AdapterView : ListView
+        listView = findViewById(R.id.listview);
+
+        // 2 - Data Source
+        String[] countries = {"USA", "Germany", "Saudi Arabia", "France" };
+
+        // 3 - Adapter: acts as a bridge between the
+        //          ' data source ' and the 'AdapterView'
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+
+                this,
+                 android.R.layout.simple_list_item_1,
+                 countries
+        );
+
+        listView.setAdapter(adapter);
+
+
+    }
+}
